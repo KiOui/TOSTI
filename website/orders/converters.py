ddee74000dcb1add49846e1cd68b8c4f0157515b
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-06-25 13:24:29 +0200

    Changed urls

--- website/orders/converters.py
+++ website/orders/converters.py
@@ -1,7 +1,7 @@
 from django.urls.converters import IntConverter
 
 from venues.models import Venue
-from .models import Shift
+from .models import Shift, Order
 
 
 class ShiftConverter(IntConverter):
@@ -50,5 +50,30 @@ class VenueConverter(IntConverter):
 
         :param obj: the Venue object
         :return: the public key of the Venue object in string format
+        """
+        return str(obj.pk)
+
+
+class OrderConverter(IntConverter):
+    """Converter for Venue model."""
+
+    def to_python(self, value):
+        """
+        Cast integer to Order.
+
+        :param value: the public key of the Order
+        :return: a Order or ValueError
+        """
+        try:
+            return Order.objects.get(id=int(value))
+        except Order.DoesNotExist:
+            raise ValueError
+
+    def to_url(self, obj):
+        """
+        Cast an object of Order to a string.
+
+        :param obj: the Order object
+        :return: the public key of the Order object in string format
         """
         return str(obj.pk)
--- website/orders/urls.py
+++ website/orders/urls.py
@@ -16,26 +16,32 @@ from .views import (
     RefreshShiftOrderView,
     RefreshProductOverviewView,
 )
-from .converters import ShiftConverter, VenueConverter
+from .converters import ShiftConverter, VenueConverter, OrderConverter
 
 register_converter(ShiftConverter, "shift")
 register_converter(VenueConverter, "venue")
-
+register_converter(OrderConverter, "order")
 
 urlpatterns = [
     path("shifts", ShiftView.as_view(), name="shifts"),
-    path("<shift:shift>/order", OrderView.as_view(), name="order"),
-    path("shifts/<venue:venue>/create", CreateShiftView.as_view(), name="shift_create"),
+    path("<shift:shift>/order-items", OrderView.as_view(), name="order"),
+    path("<venue:venue>/create", CreateShiftView.as_view(), name="shift_create"),
     path("<shift:shift>/admin", ShiftAdminView.as_view(), name="shift_admin"),
-    path("update", OrderUpdateView.as_view(), name="order_update"),
+    path("order/<order:order>/update", OrderUpdateView.as_view(), name="order_update"),
     path("<shift:shift>/products", ProductListView.as_view(), name="product_list"),
     path("<shift:shift>/overview", ShiftOverview.as_view(), name="shift_overview"),
     path(
-        "<shift:shift>/toggle", ToggleShiftActivationView.as_view(), name="shift_toggle"
+        "<shift:shift>/control/toggle",
+        ToggleShiftActivationView.as_view(),
+        name="shift_toggle",
     ),
-    path("<shift:shift>/add-time", AddShiftTimeView.as_view(), name="shift_add_time"),
     path(
-        "<shift:shift>/add-capacity",
+        "<shift:shift>/control/add-time",
+        AddShiftTimeView.as_view(),
+        name="shift_add_time",
+    ),
+    path(
+        "<shift:shift>/control/add-capacity",
         AddShiftCapacityView.as_view(),
         name="shift_add_capacity",
     ),
