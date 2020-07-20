54c7361437d33925ded06f82b126794375d5aa07
Lars van Rhijn <38525109+KiOui@users.noreply.github.com>
2020-07-20 21:33:52 +0200

    Javascript to auto-close the footer (#118)
    Co-authored-by: Job Doesburg <job.doesburg@gmail.com>

---
+++ website/orders/static/orders/js/admin-footer-close.js
@@ -0,0 +1,11 @@
+$(window).click(function() {
+    if ($('#footer-menu').hasClass('show') && $('#footer-toggler:visible').length > 0) {
+        $('#footer-toggler').click();
+    }
+});
+
+$('#footer-container').click(function(event){
+    if ($('#footer-menu').hasClass('show') && !($(event.target).parents('#footer-toggler').length > 0 || event.target.id === "footer-toggler")) {
+        event.stopPropagation();
+    }
+});
