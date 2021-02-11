efaa2c1c04bd9acb6eb1d1ce6a0b5c1843e5467a
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-02-11 09:53:15 +0100

    Flake8 fix

--- website/marietje/api/v1/views.py
+++ website/marietje/api/v1/views.py
@@ -1,7 +1,6 @@
 import spotipy
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
