1283f66879d79e4394c86306b5851f4648a68fa0
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-02-18 11:41:18 +0100

    Fix None bug in Spotipy

--- website/thaliedje/services.py
+++ website/thaliedje/services.py
@@ -82,8 +82,11 @@ def search_tracks(query, player, maximum=5):
         logging.error(e)
         raise e
 
-    result = sorted(result["tracks"]["items"], key=lambda x: -x["popularity"])
-    trimmed_result = [{"name": x["name"], "artists": [y["name"] for y in x["artists"]], "id": x["id"]} for x in result]
+    # Filter out None values as the Spotipy library might return None for data it can't decode
+    result = [x for x in result["tracks"]["items"] if x is not None]
+    result = sorted(result, key=lambda x: -x["popularity"])
+    trimmed_result = [{"name": x["name"], "artists": [y["name"] for y in x["artists"]], "id": x["id"]} for x in
+                      result]
     return trimmed_result
 
 
