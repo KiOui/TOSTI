251c568c15a6e8b7d3b287880b1e3ad80941d9cf
Job Doesburg <job.doesburg@gmail.com>
2021-10-13 09:27:32 +0200

    Fix reservation admin

--- website/venues/admin.py
+++ website/venues/admin.py
@@ -1,3 +1,4 @@
+from admin_auto_filters.filters import AutocompleteFilter
 from django.contrib import admin
 from django import forms
 from django.db.models import Q
@@ -33,12 +34,22 @@ class ReservationAdminForm(forms.ModelForm):
             self.fields["venue"].queryset = Venue.objects.filter(Q(can_be_reserved=True) | Q(pk=instance.pk))
 
 
+class ReservationAdminUserFilter(AutocompleteFilter):
+    """Filter class to filter product objects available at a certain venue."""
+
+    title = "User"
+    field_name = "user"
+
+
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """Custom admin for reservations."""
 
-    list_display = ["title", "venue", "start_time", "end_time", "user"]
-    list_filter = ["venue", "user"]
+    list_display = ["title", "venue", "association", "start_time", "end_time", "user"]
+    list_filter = ["venue", "association", "start_time", ReservationAdminUserFilter]
     search_fields = ["title"]
     date_hierarchy = "start_time"
     form = ReservationAdminForm
+
+    class Media:
+        """Necessary to use AutocompleteFilter."""
