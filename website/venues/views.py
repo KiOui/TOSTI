1d73e6fe124962785d19611e076fe57a43da88ac
Lars van Rhijn <38525109+KiOui@users.noreply.github.com>
2022-06-30 01:25:38 +0200

    Add calendar export (#232)
    * Added calendar export
    
    * Fix product_id

--- website/venues/views.py
+++ website/venues/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.models import Site
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView, ListView
+from django_ical.views import ICalFeed
 
 from tosti.filter import Filter
 from venues import services
@@ -60,3 +62,73 @@ class ListReservationsView(ListView):
     def get_queryset(self):
         """Only allow access to user's own reservations."""
         return super().get_queryset().filter(user__pk=self.request.user.pk)
+
+
+class ReservationFeed(ICalFeed):
+    """Output an iCal feed containing all reservations."""
+
+    def product_id(self, obj):
+        """Get product ID."""
+        return f"-//{Site.objects.get_current().domain}//ReservationCalendar"
+
+    def title(self, obj):
+        """Get calendar title."""
+        return "T.O.S.T.I. Reservation calendar"
+
+    def items(self, obj):
+        """Get calendar items."""
+        return Reservation.objects.filter(accepted=True).order_by("-start")
+
+    def item_title(self, item):
+        """Get item title."""
+        if item.association is not None:
+            return f"{item.association}: {item.title}"
+        else:
+            return item.title
+
+    def item_description(self, item):
+        """Get item description."""
+        return (
+            f"Title: {item.title}<br>"
+            f"Comments: {item.comment}<br>"
+            f'<a href="{self.item_link(item)}">View on T.O.S.T.I.</a>'
+        )
+
+    def item_start_datetime(self, item):
+        """Get start datetime."""
+        return item.start
+
+    def item_end_datetime(self, item):
+        """Get end datetime."""
+        return item.end
+
+    def item_link(self, item):
+        """Get item link."""
+        return "https://{}{}".format(
+            Site.objects.get_current().domain,
+            reverse("admin:venues_reservation_change", kwargs={"object_id": item.id}),
+        )
+
+    def item_location(self, item):
+        """Get item location."""
+        return f"{item.venue}"
+
+
+class ReservationVenueFeed(ReservationFeed):
+    """Output an iCal feed containing reservations for a venue."""
+
+    def product_id(self, obj):
+        """Get product ID."""
+        return f"-//{Site.objects.get_current().domain}//ReservationCalendar//{obj.name}"
+
+    def get_object(self, request, *args, **kwargs):
+        """Get the object."""
+        return kwargs.get("venue")
+
+    def items(self, obj):
+        """Get calendar items."""
+        return Reservation.objects.filter(accepted=True, venue=obj).order_by("-start")
+
+    def title(self, obj):
+        """Get calendar title."""
+        return f"T.O.S.T.I. Reservation calendar ({obj})"
