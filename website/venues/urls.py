23dbc08c267cdbe5c08bdcd05600c10a7b419ea4
Lars van Rhijn <larsvanrhijn@gmail.com>
2022-06-29 13:25:16 +0200

    Added calendar export

--- website/venues/urls.py
+++ website/venues/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, register_converter
 
 from venues import views
+from venues.converters import VenueConverter
+
+register_converter(VenueConverter, "venue")
+
 
 urlpatterns = [
     path("calendar/", views.VenueCalendarView.as_view(), name="calendar"),
     path("reservations/", views.ListReservationsView.as_view(), name="list_reservations"),
     path("add-reservation/", views.RequestReservationView.as_view(), name="add_reservation"),
+    path("reservations.ics", views.ReservationFeed(), name="ical_reservations"),
+    path("reservations-<venue:venue>.ics", views.ReservationVenueFeed(), name="ical_venue_reservations"),
 ]
