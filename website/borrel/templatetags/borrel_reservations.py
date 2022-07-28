2a5e14084af20015a8e247bbc835a2d992b52dbc
Job Doesburg <job.doesburg@gmail.com>
2022-07-28 15:32:26 +0200

    Warnings to submit borrel reservation on index.html (#318)
    * Warnings to submit borrel reservation on index.html
    
    * Fix bug for unauthenticated users

---
+++ website/borrel/templatetags/borrel_reservations.py
@@ -0,0 +1,16 @@
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+
+@register.inclusion_tag("borrel/borrel_reservation_warning.html", takes_context=True)
+def borrel_reservations_to_submit(context):
+    """Render borrel reservations to submit."""
+    user = context.get("user")
+    if not user or not user.is_authenticated:
+        return
+    reservations = user.borrel_reservations_access.filter(
+        submitted_at__isnull=True, accepted=True, start__lte=timezone.now()
+    )
+    return {"request": context.get("request"), "reservations": reservations}
