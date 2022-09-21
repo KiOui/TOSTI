2740c43d805f4b11fcffcc1027253d82fffa0145
Lars van Rhijn <larsvanrhijn@gmail.com>
2022-09-21 19:15:49 +0200

    Added a push to tantalus button on the admin screen of Shifts

--- website/tantalus/admin.py
+++ website/tantalus/admin.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
+from django.utils.html import format_html
 
+from orders.admin import ShiftAdmin
+from orders.models import Shift
 from tantalus.forms import TantalusProductAdminForm, TantalusOrderVenueAdminForm
-from tantalus.models import TantalusProduct, TantalusOrderVenue
+from tantalus.models import TantalusProduct, TantalusOrderVenue, TantalusShiftSynchronization
+from tantalus.services import synchronize_to_tantalus
 
 
 @admin.register(TantalusProduct)
@@ -24,3 +29,59 @@ class TantalusOrderVenueAdmin(admin.ModelAdmin):
         "endpoint_id",
     ]
     form = TantalusOrderVenueAdminForm
+
+
+class TantalusShiftAdmin(ShiftAdmin):
+    """Add sync to Tantalus button to ShiftAdmin."""
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        """Add context to the extra_context."""
+        try:
+            obj = Shift.objects.get(id=object_id)
+        except Shift.DoesNotExist:
+            obj = None
+
+        if extra_context is None:
+            extra_context = {}
+
+        extra_context["show_push_to_tantalus"] = obj is not None and obj.finalized
+        return super(ShiftAdmin, self).change_view(request, object_id, form_url, extra_context)
+
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+        """Add extra action to admin post request."""
+        try:
+            obj = Shift.objects.get(id=object_id)
+        except Shift.DoesNotExist:
+            obj = None
+
+        if obj and "_pushtotantalus" in request.POST:
+            if synchronize_to_tantalus(obj):
+                self.message_user(request, format_html("Tantalus synchronisation succeeded."), messages.SUCCESS)
+                if not TantalusShiftSynchronization.objects.filter(shift=obj).exists():
+                    TantalusShiftSynchronization.objects.create(shift=obj)
+            else:
+                self.message_user(
+                    request,
+                    format_html(
+                        "Failed to synchronize data to Tantalus, please consult the server logs for more information."
+                    ),
+                    level=messages.ERROR,
+                )
+            redirect_url = request.path
+            return HttpResponseRedirect(redirect_url)
+        else:
+            return super(ShiftAdmin, self).changeform_view(
+                request, object_id=object_id, form_url=form_url, extra_context=extra_context
+            )
+
+    def has_change_permission(self, request, obj=None):
+        """Don't check change permissions when _pushtotantalus is present in POST parameters."""
+        if obj and "_pushtotantalus" not in request.POST:
+            return super(TantalusShiftAdmin, self).has_change_permission(request, obj=obj)
+        else:
+            return super(TantalusShiftAdmin, self).has_change_permission(request, obj=obj)
+
+
+# Register the Tantalus Shift admin instead of the normal Shift admin.
+admin.site.unregister(Shift)
+admin.site.register(Shift, TantalusShiftAdmin)
