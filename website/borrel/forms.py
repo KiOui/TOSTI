b308a1563a627461cbd147a67afbfa9dd5e438e9
nvoers <nick-vanoers@hotmail.com>
2022-10-05 11:14:50 +0200

    added before column and less than 0 used check

--- website/borrel/forms.py
+++ website/borrel/forms.py
@@ -175,6 +175,7 @@ class ReservationItemForm(models.ModelForm):
     """A reservation item form that allows you to reserve products."""
 
     amount_after = forms.IntegerField()
+    amount_before = forms.IntegerField()
 
     class Meta:
         """Meta class for the form."""
@@ -199,8 +200,10 @@ class ReservationItemForm(models.ModelForm):
         self.fields["product_description"].disabled = True
         self.fields["product_price_per_unit"].disabled = True
 
+        self.fields["amount_before"].disabled = True
         self.fields["amount_after"].disabled = True
         self.fields["amount_used"].disabled = True
+        self.fields["amount_before"].required = False
         self.fields["amount_after"].required = False
         self.fields["amount_used"].required = False
 
@@ -221,6 +224,7 @@ class ReservationItemSubmissionForm(ReservationItemForm):
 
         product = self.instance.product
         if product and product.can_be_submitted:
+            self.fields["amount_before"].disabled = False
             self.fields["amount_after"].disabled = False
             self.fields["amount_used"].required = True
             self.fields["amount_used"].disabled = False
@@ -233,6 +237,7 @@ class ReservationItemDisabledForm(ReservationItemForm):
         """Init the form."""
         super().__init__(*args, **kwargs)
         self.fields["amount_reserved"].disabled = True
+        self.fields["amount_before"].disabled = True
         self.fields["amount_after"].disabled = True
         self.fields["amount_used"].disabled = True
 
