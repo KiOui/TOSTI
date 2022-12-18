cd3ab047fe1e220d2d95d4130511234236091dc1
Job Doesburg <job.doesburg@gmail.com>
2022-12-18 17:29:36 +0100

    Fix typo

--- website/thaliedje/crons.py
+++ website/thaliedje/crons.py
@@ -1,33 +1,32 @@
 from constance import config
 from django_cron import CronJobBase, Schedule
 
-from thaliedje import services
 from thaliedje.models import Player
 
 
 class StopMusicCronJob(CronJobBase):
     """Stop the music at a specific time every day."""
 
-    RUN_AT_TIMES = [config.THALIEDJE_STOP_MUSIC_TIME]
-    RETRY_AFTER_FAILURE_MINS = 5
+    RUN_AT_TIMES = [config.THALIEDJE_STOP_PLAYERS_AT]
+    RETRY_AFTER_FAILURE_MINS = 1
     schedule = Schedule(run_at_times=RUN_AT_TIMES, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
     code = "thaliedje.stopmusic"
 
     def do(self):
         """Stop the music."""
         for player in Player.objects.all():
-            services.player_pause(player)
+            player.spotify.pause_playback()
 
 
 class StartMusicCronJob(CronJobBase):
     """Start the music at a specific time every day."""
 
-    RUN_AT_TIMES = [config.THALIEDJE_START_MUSIC_TIME]
-    RETRY_AFTER_FAILURE_MINS = 5
+    RUN_AT_TIMES = [config.THALIEDJE_START_PLAYERS_AT]
+    RETRY_AFTER_FAILURE_MINS = 1
     schedule = Schedule(run_at_times=RUN_AT_TIMES, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
     code = "thaliedje.startmusic"
 
     def do(self):
         """Start the music."""
         for player in Player.objects.all():
-            services.player_start(player)
+            player.spotify.start_playback()
