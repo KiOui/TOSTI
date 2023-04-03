8f3063dd57e6845af150ac93eaac209881fe544c
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-04-03 12:26:13 +0200

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd "${0%/*}" || exit
-git pull
+ssh-agent bash -c 'ssh-add .ssh/id_ed25519; git pull'
 source env/bin/activate
 website/manage.py migrate
 website/manage.py collectstatic --noinput
