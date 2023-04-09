3124b81f826fa713901653ea7564a47e52032850
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-04-09 21:30:18 +0200

    Remove push on submit for shifts

--- website/silvasoft/apps.py
+++ website/silvasoft/apps.py
@@ -4,11 +4,3 @@ from django.apps import AppConfig
 class SilvasoftConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "silvasoft"
-
-    def ready(self):
-        """
-        Ready method.
-
-        :return: None
-        """
-        from silvasoft import signals  # noqa
