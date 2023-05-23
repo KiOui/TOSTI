38ce9a58a7b62d9e9d23b0995bb3f3c885204d1f
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-05-23 16:00:53 +0200

    Fix venue card color and user staff not updating

--- website/users/signals.py
+++ website/users/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, m2m_changed, pre_save
 from django.dispatch import receiver
 
 from users.models import GroupSettings
@@ -19,7 +19,5 @@ def after_group_change(sender, instance, **kwargs):
 def after_user_groups_changed(sender, instance, **kwargs):
     """Update the is_staff value of the users when they are added to groups."""
     action = kwargs.get("action")
-    pk_set = kwargs.get("pk_set")
     if action == "post_add" or action == "post_remove":
-        for user in User.objects.filter(pk__in=pk_set):
-            update_staff_status(user)
+        update_staff_status(instance)
