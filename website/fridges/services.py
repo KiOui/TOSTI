054a1064409f061613ed2cee2bf1d0e5914526ea
Job Doesburg <job.doesburg@gmail.com>
2023-11-16 21:57:55 +0100

    Add daily opening requirement (#521)
    * Add daily opening requirement
    
    * Easier code
    
    * Add docs
    
    * Do check if feature flag is enabled
    
    ---------
    
    Co-authored-by: Lars van Rhijn <larsvanrhijn@gmail.com>

--- website/fridges/services.py
+++ website/fridges/services.py
@@ -1,4 +1,6 @@
+from constance import config
 from django.db.models import Q
+from django.utils import timezone
 
 from age.services import verify_minimum_age
 from fridges.models import AccessLog
@@ -24,9 +26,14 @@ def user_can_open_fridge(user, fridge):
         return False, None
 
     opening_hours = fridge.current_opening_hours
-
     if not opening_hours.exists():
         return False, None
+
+    if config.FRIDGE_REQUIRE_DAILY_OPENING:
+        # This requires a daily opening by a user with the open_always permission.
+        already_opened_today = AccessLog.objects.filter(fridge=fridge, timestamp__date=timezone.now().date()).exists()
+        if not already_opened_today:
+            return False, None
 
     if opening_hours.filter(Q(restrict_to_groups__in=user.groups.all()) | Q(restrict_to_groups__isnull=True)).exists():
         return True, fridge.unlock_for_how_long
