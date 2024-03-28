d90558f0d65211a6a593bf31cfc27d07344383ed
Job Doesburg <job.doesburg@gmail.com>
2024-03-28 09:34:44 +0100

    Dont overwrite

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python manage.py migrate --noinput
-echo "${SAML_PUBLIC_KEY_FILE}" >> /app/tosti/saml/public.cert
-echo "${SAML_PRIVATE_KEY_FILE}" >> /app/tosti/saml/private.key
-echo "${SAML_METADATA_FILE}" | base64 -d >> /app/tosti/saml/metadata.xml
+echo "${SAML_PUBLIC_KEY_FILE}" > /app/tosti/saml/public.cert
+echo "${SAML_PRIVATE_KEY_FILE}" > /app/tosti/saml/private.key
+echo "${SAML_METADATA_FILE}" | base64 -d > /app/tosti/saml/metadata.xml
 uwsgi --http :80 --wsgi-file /app/tosti/wsgi.py --master --processes 4 --threads 2 --uid nobody --gid nogroup --disable-logging --static-map ${DJANGO_STATIC_URL}=${DJANGO_STATIC_ROOT} --static-map ${DJANGO_MEDIA_URL}=${DJANGO_MEDIA_ROOT}
