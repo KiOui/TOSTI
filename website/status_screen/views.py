fe199fb4907aa1ffbd6fe35354cd2783541d8b0c
Lars van Rhijn <lars@radix-security.com>
2024-06-03 16:38:13 +0200

    Add redirect view to redirect to current status screen

--- website/status_screen/views.py
+++ website/status_screen/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 from django.views.generic import TemplateView
+
+from orders.templatetags.start_shift import currently_active_shift_for_venue
 
 
 class StatusScreen(TemplateView):
@@ -11,3 +16,16 @@ class StatusScreen(TemplateView):
         """GET request for status screen view."""
         shift = kwargs.get("shift")
         return render(request, self.template_name, {"shift": shift})
+
+
+class VenueRedirectView(View):
+    """Redirect to the current shift of a venue."""
+
+    def get(self, request, **kwargs):
+        """Redirect a user to the active status screen of a shift."""
+        venue = kwargs.get("venue")
+        shift = currently_active_shift_for_venue(venue)
+        if shift is None:
+            raise Http404()
+        else:
+            return redirect(reverse("status_screen:status", kwargs={"shift": shift}))
