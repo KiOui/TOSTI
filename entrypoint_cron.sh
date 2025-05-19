eb304c7ae1a00de9fb5bd98a98d11972c03e324d
Job Doesburg <job.doesburg@gmail.com>
2025-05-19 22:35:33 +0200

    Fix cron entrypoint

--- entrypoint_cron.sh
+++ entrypoint_cron.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 export > /etc/env_vars.sh
-echo "*/1 * * * * root . /etc/env_vars.sh && /usr/local/bin/python /app/manage.py runcrons --silent >> /var/log/cron.log 2>&1" > /etc/cron.d/cron-schedule
+
+chown -R nobody:nogroup /app/cache
+
+# Create the cron job but have it run as the nobody user
+echo "*/1 * * * * nobody . /etc/env_vars.sh && /usr/local/bin/python /app/manage.py runcrons --silent >> /var/log/cron.log 2>&1" > /etc/cron.d/cron-schedule
 chmod 0644 /etc/cron.d/cron-schedule
 crontab /etc/cron.d/cron-schedule
 touch /var/log/cron.log
+chmod 666 /var/log/cron.log
 
 # Start cron in the background
 cron
