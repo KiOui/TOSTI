04d3e4b5442b8d0fef12339ed30c6e991f96d90e
Job Doesburg <job.doesburg@gmail.com>
2025-06-28 11:26:52 +0200

    Fix

--- website/tosti/context_processors.py
+++ website/tosti/context_processors.py
@@ -24,5 +24,8 @@ def footer_credits(request):
 
 def minimum_registered_age(request):
     """Context processor to check if the user has a minimum registered age."""
+    if not request.user.is_authenticated:
+        return {"minimum_registered_age": False}
+
     age = get_minimum_age(request.user)
     return {"minimum_registered_age": age is not None}
