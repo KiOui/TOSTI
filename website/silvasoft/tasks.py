aa58272f69acf19ff6f0059d8600a30e14361d57
Lars van Rhijn <larsvanrhijn@gmail.com>
2026-06-04 19:31:37 +0200

    Make arguments of the Celery tasks serializable

--- website/silvasoft/tasks.py
+++ website/silvasoft/tasks.py
@@ -18,10 +18,20 @@ from tosti.metrics import emit as emit_metric
 
 
 @shared_task
-def task_synchronize_shift_to_silvasoft(shift: Shift):
-    """Celery task wrapper for synchronizing a Shift to Silvasoft."""
+def task_synchronize_shift_to_silvasoft(shift_pk: int):
+    """
+    Celery task wrapper for synchronizing a Shift to Silvasoft.
+
+    This tasks takes the ID of the object because the arguments need to be serializable.
+    """
+    try:
+        shift = Shift.objects.get(pk=shift_pk)
+    except Shift.DoesNotExist:
+        emit_metric("cron_silvasoft_shift_sync_run", skipped_reason="id_does_not_exist")
+        return
+
     if not SilvasoftClient.can_create_client():
-        emit_metric("cron_silvasoft_sync_run", skipped_reason="no_client")
+        emit_metric("cron_silvasoft_shift_sync_run", skipped_reason="no_client")
         return
 
     silvasoft_invoice, _ = SilvasoftShiftInvoice.objects.get_or_create(shift=shift)
@@ -44,11 +54,23 @@ def task_synchronize_shift_to_silvasoft(shift: Shift):
 
 @shared_task
 def task_synchronize_borrel_reservation_to_silvasoft(
-    borrel_reservation: BorrelReservation,
+    borrel_reservation_pk: int,
 ):
-    """Celery task wrapper for synchronizing a Borrel Reservation to Silvasoft."""
+    """
+    Celery task wrapper for synchronizing a Borrel Reservation to Silvasoft.
+
+    This tasks takes the ID of the object because the arguments need to be serializable.
+    """
+    try:
+        borrel_reservation = BorrelReservation.objects.get(pk=borrel_reservation_pk)
+    except Shift.DoesNotExist:
+        emit_metric("cron_silvasoft_shift_sync_run", skipped_reason="id_does_not_exist")
+        return
+
     if not SilvasoftClient.can_create_client():
-        emit_metric("cron_silvasoft_sync_run", skipped_reason="no_client")
+        emit_metric(
+            "cron_silvasoft_borrel_reservation_sync_run", skipped_reason="no_client"
+        )
         return
 
     silvasoft_invoice, _ = SilvasoftBorrelReservationInvoice.objects.get_or_create(
